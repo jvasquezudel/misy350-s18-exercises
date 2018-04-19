2b7d6f04fe98d2de41ca2298717968798650509c
Jose Vasquez <jvasquez@udel.edu>
2018-04-19 10:24:19 -0400

    ptyhon code

---
+++ 4-19-18/python-basics.py
@@ -0,0 +1,12 @@
+# this is python code
+greeting = "hello world"
+print greeting  #this prints out a string
+
+print len(greeting)
+print greeting.upper()
+print greeting[4]
+
+greet = "hello"
+name = "Jose"
+print greet + " " + name
+print "%s %s, how are you today?" % (greet, name)
