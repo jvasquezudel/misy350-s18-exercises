e9d3aa5f947a7f0a98a333c191cf9cf17a09e4bd
Jose Vasquez <jvasquez@udel.edu>
2018-05-07 00:05:14 -0400

    my app revised

--- songbase/songbase.py
+++ songbase/songbase.py
@@ -6,13 +6,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/form-basics')
+def form_basics():
+    return render_template('form-basics.html')
+
+@app.route('/form-demo')
+def form_demo():
+    first_name = request.args.get('first_name')
+    return first_name
+
+
 @app.route('/user')
 def user():
     return '<h1> The page for the users </h1>'
 
 @app.route('/user/<string:username>')
 def username(username):
-    return render_template('template.html')
+    return render_template('template.html', username = username)
 
 
 if __name__ == '__main__':
